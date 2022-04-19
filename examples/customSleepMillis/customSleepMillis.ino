3719c0b2e0b8fecdb4b6dbe1cfe44034cd4ecedb
Tim <sakunamary@gmail.com>
2022-04-19 15:48:02 +0800

    update comment with English and Chinese version
    update comment with English and Chinese version

--- examples/customSleepMillis/customSleepMillis.ino
+++ examples/customSleepMillis/customSleepMillis.ino
@@ -4,31 +4,37 @@
 
 #include <GyverPower.h>
 
-uint32_t myMillis = 0;  // наш миллис
+uint32_t myMillis = 0;  // наш миллис  //our millis //设置myMillis初始化值
 
 void setup() {
-  power.autoCalibrate(); // автоматическая калибровка
+  power.autoCalibrate(); // автоматическая калибровка  //automatic calibration  //自动校准timer值
 }
 
 void loop() {
   static uint32_t tmr1, tmr2;
 
-  // "таймер" на 2 секунды
+  // "таймер" на 2 секунды  //"timer" for 2 seconds  //设置“timer” 的值为2s
   if (myMillis - tmr1 > 2000) {
     tmr1 += 2000;
-    // действие 1
+    // действие 1 action 1 动作1
   }
 
-  // "таймер" на 10 секунд
+  // "таймер" на 10 секунд //"timer" for 10 seconds  //设置“timer” 的值为10s
   if (myMillis - tmr2 > 10000) {
     tmr2 += 10000;
-    // действие 2
+    // действие 2  //action 2 //动作2
   }
 
-  // спим 1 секунду (1000 мс)
-  // sleepDelay возвращает остаток сна в мс, если он есть
+  // спим 1 секунду (1000 мс) 
+  //sleep 1 second (1000ms) 
+  //睡眠1s（1000ms）
+  // sleepDelay возвращает остаток сна в мс, если он есть 
+  //sleepDelay returns the rest of the sleep in ms, if any 
+  //sleepDelay 返回剩余的睡眠时间（ms），如果有的话
   byte left = power.sleepDelay(1000);
 
-  // прибавляем и корректируем наш миллис
+  // прибавляем и корректируем наш миллис//adding and adjusting our millis
+  // adding and adjusting our millis
+  //增加和调整 myMillis
   myMillis += 1000 - left;
 }
